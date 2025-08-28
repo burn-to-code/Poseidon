03d76d627bbfa400b7aa71fdcef0acbef5145b8f
Dylan Senasson <dylan.senasson@outlook.fr>
2025-08-28 20:29:43 +0200

    Suppression de warning au lancement de l'aapplication :
    suppression d'un double beans pour charger l'utilisateur sur spring security
    
    suppression du mdp de la base de données en laissant une variable d'environnement
    
    suppression d'un ligne déprecié par hibernates : spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect

--- src/main/java/com/poseidon/config/SpringSecurityConfig.java
+++ src/main/java/com/poseidon/config/SpringSecurityConfig.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -44,18 +40,6 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER").build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
